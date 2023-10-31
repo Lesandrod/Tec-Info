6047060d242d86518b7979d6d075fa3efae110d0
Lesandrod <lentrix12345@gmail.com>
2023-10-30 23:36:42 -0500

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
 
   let fechaFormateada = '';
 
+
+  //opcion para formatear la fecha 
   if (student && student.fecNacimiento) {
     const fechaNacimiento = student.fecNacimiento;
     if (Array.isArray(fechaNacimiento) && fechaNacimiento.length >= 3) {
@@ -58,35 +60,38 @@ function App() {
             <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-100 ">
               <img className="object-cover w-49 rounded-t-lg p-2  md:h-auto  md:rounded-none md:rounded-l-lg" src={code} alt="Foto del alumno" width={247} />
               <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{student.fullname}</h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{student.fullname}</h5>
+                <p className="mb-3 font-normal text-gray-700 ">
                   <BsShieldCheck size={22} className="inline-block mr-5" /> {student.especialidad}
                 </p>
 
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <AiOutlineMail size={22} className="inline-block mr-5" /> {student.correoInstitucional}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <BiCake size={22} className="inline-block mr-5" /> {fechaFormateada}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <HiOutlineIdentification size={22} className="inline-block mr-5" /> {student.numDocumento}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <AiOutlineHome size={22} className="inline-block mr-5" /> {student.dirDomicilio}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <BsTelephone size={22} className="inline-block mr-5" /> {student.telCelular}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 ">
                   <BiRadar size={22} className="inline-block mr-5" />   Ciclo {student.ciclo}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{student.sexo === 'M' ? <p><AiOutlineMan size={22} className="inline-block mr-5" /> Man</p> : <p><AiOutlineWoman size={22} className="inline-block mr-2" /> Woman</p>} </p>
+                <p className="mb-3 font-normal text-gray-700 ">{student.sexo === 'M' ? <p><AiOutlineMan size={22} className="inline-block mr-5" /> Man</p> : <p><AiOutlineWoman size={22} className="inline-block mr-2" /> Woman</p>} </p>
               </div>
             </div>
           </div>
         )}
       </div>
+      <div>
+      
+    </div>
 
 
 
